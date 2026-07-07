2d29ce2a6b26e2466164db0fa5f67be7667f5a5e
errjace <errjace@users.noreply.github.com>
2026-07-07 14:41:13 +0200

    Fix routing for Render

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(path.join(__dirname)));
+const io = new Server(server, {
+  cors: { origin: '*', methods: ['GET', 'POST'] }
+});
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'amici.html'));
+});
+
+app.get('/amici.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'amici.html'));
 });
 
